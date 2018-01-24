cb7652f144cf85906bf98a5e5c9a625b9828e77b
Stoyan Konstantinov <skonstantinov89@users.noreply.github.com>
2018-01-24 17:51:53 +0200

    Added some new solution code

--- solution/weather_console.py
+++ solution/weather_console.py
@@ -1,5 +1,5 @@
 
-
+from pprint import pprint
 import requests, json, codecs, random
 
 API_KEY = '&APPID=5c55042b76165ad5321007003915fc66'
@@ -8,22 +8,19 @@ city_dict = {}
 city_list = []
 random_city_list = []
 
-def load_city_list():
-    x = json.load(codecs.open('city.list.json', 'r', 'utf-8'))
-    for each in x:
-        city_dict[each['id']] = each['name']
-        city_list.append(each['id'])
+result_list = []
 
+def load_city_list():
+    city_data = json.load(codecs.open('city.list.json', 'r', 'utf-8'))
+    for eachCity in city_data:
+        city_dict[eachCity['id']] = eachCity['name']
+        city_list.append(eachCity['id'])
 
 def get_weather_for_city(city):
-    base_request_url = 'weather?q=' + city
+    print (city_dict[city])
+    base_request_url = 'weather?q=' + city_dict[city] + '&units=metric'
     response = requests.get(URL+base_request_url+API_KEY)
     return response.json()
-
-# current_city_data = get_weather_for_city(city='Sofia').json()
-# from pprint import pprint
-# pprint (current_city_data['main'])
-
 
 def build_random_city_list(count):
     for _ in range(count):
@@ -32,7 +29,12 @@ def build_random_city_list(count):
 
 load_city_list()
 build_random_city_list(5)
-print (random_city_list)
+for eachCity in random_city_list:
+    cityData = get_weather_for_city(eachCity)
+    print ("Град: %s" %cityData['name'])
+    print ("Tемпература в момента: %s" %cityData['main']['temp'])
+    print ("Град: %s" %cityData['name'])
+    raise
 
 
 
